1a9ca4bf0b30752c6de038c4a97e139fa7566e39
Effervescent808 <HaufschildTyson@gmail.com>
2025-02-12 14:57:23 -0700

    SHAencrypt.py

--- SHAencrypt.py
+++ SHAencrypt.py
@@ -1,6 +1,8 @@
 import os
 import hashlib
 
+print (os.getcwd())
+
 #read contents of the file
 file = open('file.txt', 'rb')
 contents = file.read()
@@ -10,3 +12,18 @@ file.close()
 key = hashlib.sha256(contents).hexdigest()
 print('Key:',key)
 
+key1 = key[:32]
+key2 = key[32:]
+
+
+
+
+
+
+
+
+
+#test prints
+print(key)
+print(key1, key2)
+print(len(key1), len(key2))
