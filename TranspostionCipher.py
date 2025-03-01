df315ef2023da7a0965a2a21e281dc1b25be04e2
Effervescent808 <HaufschildTyson@gmail.com>
2025-03-01 12:59:45 -0700

    TranspostionCipher.py

--- TranspostionCipher.py
+++ TranspostionCipher.py
@@ -1,5 +1,7 @@
 import math
 
+TestMode = False
+
 #Get Plaintext and Key
 plaintext = input('Enter plaintext: ').replace(" ","")
 key = input('key:')
@@ -8,39 +10,41 @@ if not key:
     print("Invalid key. Key cannot be empty.")
     exit()
 
-#create sequence + grid
+#Create sequence + grid
 sequence = ''.join(sorted(key, key=str.lower))
 grid = math.ceil(len(plaintext)/len(key))
 
-#split plaintext based on key
+#Split plaintext based on key
 rows = [plaintext[i * len(key):(i + 1) * len(key)] for i in range(grid)]
 row_dict = {f"row{index}": row for index, row in enumerate(rows, start=1)}
 
-#get column 1
-column1= ""
-for i in range(1, grid+1):
-    column1 += (row_dict[f"row{i}"][0])
+#Pull columns
+columns = [''.join(row[i] for row in rows if i < len(row)) for i in range(len(key))]
+Columns = ''.join(columns)
 
-#get column 2
-column2= ""
-for i in range(1, grid+1):
-    column2 += (row_dict[f"row{i}"][1])
+#Rearrange Columns
+sort = sorted(range(len(key)), key=lambda i: key[i].lower())
+Jargan = [''.join(columns[i] for i in sort)]
+sortedcolumns = ''.join(Jargan)
 
-#get column 3
-column3= ""
-for i in range(1, grid+1):
-    column3 += (row_dict[f"row{i}"][2])
+if TestMode == False:
+    print()
+    print('Scramble:\n',sortedcolumns)
 
 #test prints
-print()
-print('Plaintext:',plaintext)
-print()
-print(key)
-print(sequence)
-print()
-for i in range(1, grid+1):
-    print(row_dict[f'row{i}'])
-print()
-print(grid)
-print()
-print(column1,column2,column3, sep="")
+if TestMode == True:
+    print()
+    print('Plaintext:',plaintext)
+    print()
+    print('Key:',key)
+    print('Alphabetized Key:',sequence)
+    print()
+    for i in range(1, grid+1):
+        print(row_dict[f'row{i}'])
+    print()
+    print(grid)
+    print()
+    print(Columns)
+    print()
+    print(sort)
+    print(sortedcolumns)
